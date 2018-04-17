735b854942f8e435d2b033f1fbdfd6814392d2a5
algori11 <37229404+algori11@users.noreply.github.com>
2018-04-17 19:22:50 +0900

    Bit-zのメジャーな通貨の最小単位の情報を追記

--- ccxt_extrainfo.py
+++ ccxt_extrainfo.py
@@ -4,4 +4,17 @@ class info:
     def minqty(self):
         minqdict = {}
         minqdict["bitz"] = {"DASH/BTC": 0.005}
+        minqdict["bitz"] = {"EOS/BTC": 1.0}
+        minqdict["bitz"] = {"TRX/BTC": 2050}
+        minqdict["bitz"] = {"ETH/BTC": 0.05}
+        minqdict["bitz"] = {"LTC/BTC": 0.1}
+        minqdict["bitz"] = {"EKT/BTC": 100}
+        minqdict["bitz"] = {"ETC/BTC": 0.5}
+        minqdict["bitz"] = {"LSK/BTC": 0.2}
+        minqdict["bitz"] = {"NULS/BTC": 10}
+        minqdict["bitz"] = {"ZEC/BTC": 0.05}
+        minqdict["bitz"] = {"MCO/BTC": 0.05}
+        minqdict["bitz"] = {"QTUM/BTC": 0.01}
+        minqdict["bitz"] = {"TRX/BTC": 2050}
+        
         return minqdict
