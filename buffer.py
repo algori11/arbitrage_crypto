87dd3f25e0a171968a85698701f1a62ffb89d7fc
algori11 <37229404+algori11@users.noreply.github.com>
2018-04-26 15:59:08 +0900

    並列処理の実装／各取引所への適応モジュールbuffer.pyの追加

---
+++ buffer.py
@@ -0,0 +1,45 @@
+"""
+取引所ごとに発生する問題を解決するためのモジュール
+取引所のインスタンスを生成した直後にbuffer.buffer()の中に放り込んで適当な修飾を加える
+"""
+
+# スイッチ
+def buffer(ts, passwords):
+    if ts.name == "binance":
+        return buff_binance(ts)
+    if ts.name == "hitbtc2":
+        return buff_hitbtc2(ts)
+    if ts.name == "bitz":
+        return buff_bitz(ts, passwords)
+    if ts.name == "yobit":
+        return buff_yobit(ts)
+    else:
+        return ts
+
+def buff_binance(ts):
+    # PC時計とBinanceのサーバー時計のズレが原因でおきるエラーを回避
+    ts.options["adjustForTimeDifference"] = True
+    return ts
+
+def buff_hitbtc2(ts):
+    #今のところ特になし
+    pass
+    return ts
+
+def buff_bitz(ts, passwords):
+    # 取引パスワードを追加
+    try:
+        ts.password = passwords["bitz"]
+        return ts
+    except:
+        print("Please fill PASS in config.ini")
+        raise
+
+def buff_yobit(ts):
+    # nonceを1秒よりはやい頻度で更新するよう再定義（未検証）
+    ts.nonce = lambda :ccxt.Exchange.milliseconds()%1000000000
+    return ts
+
+
+
+
